1c64afcfe53dd4a5a3f77eb814f252548bfeba22
eun-choi <54615424+eun-choi@users.noreply.github.com>
2020-10-13 14:31:22 +0900

    update 10/13

--- bin/templates/cordova/lib/retry.js
+++ bin/templates/cordova/lib/retry.js
@@ -6,9 +6,7 @@
     to you under the Apache License, Version 2.0 (the
     "License"); you may not use this file except in compliance
     with the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
+    http://www.apache.org/licenses/LICENSE-2.0
     Unless required by applicable law or agreed to in writing,
     software distributed under the License is distributed on an
     "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,7 +19,7 @@
 
 var events = require('cordova-common').events;
 
-/*
+/**
  * Retry a promise-returning function a number of times, propagating its
  * results on success or throwing its error on a failed final attempt.
  *
@@ -31,31 +29,13 @@ var events = require('cordova-common').events;
  *
  * @returns {Promise}
  */
-module.exports.retryPromise = function (attemptsLeft, promiseFunction) {
-    // NOTE:
-    //      get all trailing arguments, by skipping the first two (attemptsLeft and
-    //      promiseFunction) because they shouldn't get passed to promiseFunction
-    var promiseFunctionArguments = Array.prototype.slice.call(arguments, 2);
-
-    return promiseFunction.apply(undefined, promiseFunctionArguments).then(
-        // on success pass results through
-        function onFulfilled (value) {
-            return value;
-        },
-
-        // on rejection either retry, or throw the error
-        function onRejected (error) {
-            attemptsLeft -= 1;
-
-            if (attemptsLeft < 1) {
-                throw error;
-            }
-
+module.exports.retryPromise = async function (attemptsLeft, promiseFunction, ...args) {
+    while (true) {
+        try {
+            return await promiseFunction(...args);
+        } catch (error) {
+            if (--attemptsLeft < 1) throw error;
             events.emit('verbose', 'A retried call failed. Retrying ' + attemptsLeft + ' more time(s).');
-
-            // retry call self again with the same arguments, except attemptsLeft is now lower
-            var fullArguments = [attemptsLeft, promiseFunction].concat(promiseFunctionArguments);
-            return module.exports.retryPromise.apply(undefined, fullArguments);
         }
-    );
+    }
 };
